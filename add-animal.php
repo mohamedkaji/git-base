46fab6261ec533e45b4d24e77bb15be6168c66d0
mohamedkaji <92440516+mohamedkaji@users.noreply.github.com>
2022-06-22 16:44:38 +0200

    Update add-animal.php

--- add-animal.php
+++ add-animal.php
@@ -30,7 +30,7 @@ function uploadFile($paramName)
 // }
 
 //on insère les données dans la BDD
-require "db/connexion.php";
+// require "db/connexion.php";
 try {
   // Créér une connexion entre PHP et la base de données
   $connexion2 = new Connexion();
@@ -168,22 +168,6 @@ try {
      <div class="tail"></div>
 
    </div>
-
-   
-<!-- about //
-<div class="about">
-  <a class="bg_links social portfolio" href="https://www.rafaelalucas.com" target="_blank">
-     <span class="icon"></span>
-  </a>
-  <a class="bg_links social dribbble" href="https://dribbble.com/rafaelalucas" target="_blank">
-     <span class="icon"></span>
-  </a>
-  <a class="bg_links social linkedin" href="https://www.linkedin.com/in/rafaelalucas/" target="_blank">
-     <span class="icon"></span>
-  </a>
-  <a class="bg_links logo"></a>
-</div>    -->
-<!-- end about -->
 
 <div class="content">
    <div class="cat2">
